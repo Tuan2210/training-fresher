e3286fabb744f01cadadaeb6fd927fe7b563aa38
Đinh Quang Tuấn <quangtuan496@gmail.com>
2023-12-29 15:39:04 +0700

    animation loading

--- week05/weather-app/main.js
+++ week05/weather-app/main.js
@@ -1,5 +1,60 @@
 const apiKey = "60836d7502891317b3d0942b4f1d416b";
 
+function animationTypingTxt() {
+  var words = ["Vui lòng chờ trong giây lát"],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 20;
+
+  const loading = document.querySelector(".loading");
+  loading.classList.remove("hidden");
+  loading.classList.add("flex");
+
+  const weatherInfoContainer = document.querySelector(".detailsInfo");
+  weatherInfoContainer.classList.remove("flex");
+  weatherInfoContainer.classList.add("hidden");
+
+  function animate() {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    } else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substring(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      } else {
+        offset--;
+      }
+    }
+    document.querySelector(".lblLoading").textContent = part;
+
+    if (offset <= words[i].length) {
+      requestAnimationFrame(animate);
+    }
+  }
+
+  animate();
+}
+
 //--select place--
 function getPlaces() {
   axios
@@ -35,7 +90,7 @@ function getCoordinates() {
   function apiCoordinates(place) {
     axios
       .get(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${place},VN&appid=${apiKey}`
+        `https://api.openweathermap.org/geo/1.0/direct?q=${place},VN&appid=${apiKey}`
       )
       .then(function (res) {
         res.data.map((placeInfo) => {
@@ -64,31 +119,41 @@ function getWeather(lat, lon) {
 
 //--display weahter info--
 function displayWeatherInfo(weatherInfo) {
-  const weatherInfoContainer = document.querySelector(".wrapper__weather-info");
+  const weatherInfoContainer = document.querySelector(".detailsInfo"),
+    loading = document.querySelector(".loading");
 
-  weatherInfoContainer.innerHTML = "";
+  animationTypingTxt();
 
-  const temperature = (weatherInfo.current.temp * 0.1).toFixed();
-  const description = weatherInfo.current.weather[0].description;
-  const humidity = weatherInfo.current.humidity;
-  const windSpeed = weatherInfo.current.wind_speed.toFixed();
+  setTimeout(() => {
+    loading.classList.remove("flex");
+    loading.classList.add("hidden");
 
-  const temperatureElement = document.createElement("p");
-  temperatureElement.textContent = `Temperature: ${temperature}°C`;
+    weatherInfoContainer.innerHTML = "";
+    weatherInfoContainer.classList.remove("hidden");
+    weatherInfoContainer.classList.add("flex");
 
-  const descriptionElement = document.createElement("p");
-  descriptionElement.textContent = `Description: ${description}`;
+    const temperature = (weatherInfo.current.temp * 0.1).toFixed();
+    const description = weatherInfo.current.weather[0].description;
+    const humidity = weatherInfo.current.humidity;
+    const windSpeed = weatherInfo.current.wind_speed.toFixed();
 
-  const humidityElement = document.createElement("p");
-  humidityElement.textContent = `Humidity: ${humidity}`;
+    const temperatureElement = document.createElement("p");
+    temperatureElement.textContent = `Temperature: ${temperature}°C`;
 
-  const windSpeedElement = document.createElement("p");
-  windSpeedElement.textContent = `Wind speed: ${windSpeed} km/h`;
+    const descriptionElement = document.createElement("p");
+    descriptionElement.textContent = `Description: ${description}`;
 
-  weatherInfoContainer.appendChild(temperatureElement);
-  weatherInfoContainer.appendChild(descriptionElement);
-  weatherInfoContainer.appendChild(humidityElement);
-  weatherInfoContainer.appendChild(windSpeedElement);
+    const humidityElement = document.createElement("p");
+    humidityElement.textContent = `Humidity: ${humidity}`;
+
+    const windSpeedElement = document.createElement("p");
+    windSpeedElement.textContent = `Wind speed: ${windSpeed} km/h`;
+
+    weatherInfoContainer.appendChild(temperatureElement);
+    weatherInfoContainer.appendChild(descriptionElement);
+    weatherInfoContainer.appendChild(humidityElement);
+    weatherInfoContainer.appendChild(windSpeedElement);
+  }, 2000);
 }
 
 //--animation bubble bg--
@@ -133,6 +198,7 @@ document.addEventListener("DOMContentLoaded", function () {
     setTimeout(() => {
       selectPlace.addEventListener("change", () => {
         locationIcon.classList.remove("animateLocation");
+        animationTypingTxt();
       });
     }, 0);
   });
