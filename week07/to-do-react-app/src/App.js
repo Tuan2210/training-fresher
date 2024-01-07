885821c820e95aca5da6bb45aa8ae68080565e98
Đinh Quang Tuấn <quangtuan496@gmail.com>
2024-01-07 18:44:06 +0700

    fix routes

---
+++ week07/to-do-react-app/src/App.js
@@ -0,0 +1,13 @@
+import { RouterProvider } from "react-router-dom";
+
+import { publicRoutes } from "./routes";
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={publicRoutes} />
+    </>
+  );
+}
+
+export default App;
