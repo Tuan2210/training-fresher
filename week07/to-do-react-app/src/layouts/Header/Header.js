6f9bc93aad105bde90fd713ca74d3cffe17db056
Đinh Quang Tuấn <quangtuan496@gmail.com>
2024-01-08 14:36:50 +0700

    done layouts

--- week07/to-do-react-app/src/layouts/Header/Header.js
+++ week07/to-do-react-app/src/layouts/Header/Header.js
@@ -3,21 +3,70 @@ import classNames from "classnames/bind";
 
 import LinkBtn from "../../components/ui/LinkBtn.js";
 
+import { useState, useEffect } from "react";
+
 const cx = classNames.bind(styles);
 
 export default function Header() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formatTime = () => {
+    const day = currentTime.getDay();
+    const date = currentTime.getDate();
+    const month = currentTime.getMonth();
+    const year = currentTime.getFullYear();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    const seconds = currentTime.getSeconds();
+
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const months = [
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "11",
+      "12",
+    ];
+
+    const formattedTime = `${days[day]} | ${date}/${months[month]}/${year}`;
+    const formattedClock =
+      hours < 10
+        ? `0${hours}`
+        : hours +
+          `:${minutes < 10 ? `0${minutes}` : minutes}:${
+            seconds < 10 ? `0${seconds}` : seconds
+          }`;
+
+    return { formattedTime, formattedClock };
+  };
+
+  const { formattedTime, formattedClock } = formatTime();
+
   return (
     <header className="w-full h-20 p-3 flex justify-between">
-      {/* logo */}
-      {/* <a href="#" className="relative flex text-center">
-        <img
-          src="/assets/imgs/taskobey_line.png"
-          alt="logo"
-          width="200"
-          height="200"
-        />
-      </a> */}
-
       {/* link-btns */}
       <div className={cx(["", "flex justify-center items-center gap-5"])}>
         <LinkBtn to="/" label="Home" className="HomeBtn" />
@@ -80,7 +129,10 @@ export default function Header() {
 
       {/* time-box */}
       <div
-        className={cx(["time-box", "flex flex-row text-white font-bold gap-4"])}
+        className={cx([
+          "time-box",
+          "w-72 flex flex-row text-white font-bold gap-4",
+        ])}
       >
         {/* day-time-box */}
         <div
@@ -90,14 +142,14 @@ export default function Header() {
             className="fa-solid fa-calendar-days"
             style={{ color: "#068995" }}
           />
-          <div id="day-time"></div>
+          <div id="day-time">{formattedTime}</div>
         </div>
         {/* clock-time-box */}
         <div
           className={cx(["clock-time-box", "flex flex-row items-center gap-4"])}
         >
           <i className="fa-regular fa-clock" style={{ color: "#068995" }} />
-          <div id="clock-time"></div>
+          <div id="clock-time">{formattedClock}</div>
         </div>
       </div>
     </header>
