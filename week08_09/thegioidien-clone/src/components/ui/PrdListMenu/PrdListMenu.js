6d11f701dab38223dc9e0a515599f043416546a5
Đinh Quang Tuấn <quangtuan496@gmail.com>
2024-01-11 12:09:18 +0700

    Update PrdListMenu.js

--- week08_09/thegioidien-clone/src/components/ui/PrdListMenu/PrdListMenu.js
+++ week08_09/thegioidien-clone/src/components/ui/PrdListMenu/PrdListMenu.js
@@ -15,37 +15,45 @@ import {
 
 export default function PrdListMenu() {
   const [isDisplay, setIsDisplay] = useState("none");
-  const [isDisplaySub, setIsDisplaySub] = useState("none");
-
-  function FirstSubMenu({ item }) {
-    // const [isOpen, setIsOpen] = useState(false);
-
-    // const handleToggle = () => {
-    //   setIsOpen(true);
-    //   setIsDisplaySub(isDisplaySub === "none" ? "block" : "none");
-    // };
-
-    return (
-      // {item.subMenu.map((subItem, index) => (
-      //   <SubMenuItem
-      //     key={index}
-      //     item={subItem}
-      //     level={level + 1}
-      //     // toggleSubMenu={toggleSubMenu}
-      //   />
-      // ))}
-      <CDropdown
-        component="div"
-        className="w-full h-full text-white bg-[#48322A]"
-      >
-        <CDropdownToggle>{item.title}</CDropdownToggle>
-        <CDropdownMenu></CDropdownMenu>
-      </CDropdown>
-    );
-  }
-
   function handleClickListItem() {
     setIsDisplay(isDisplay === "none" ? "flex" : "none");
+  }
+
+  // const [isDisplaySub, setIsDisplaySub] = useState("none");
+  // function handleClickSubListItem() {
+  //   setIsDisplaySub(isDisplaySub === "none" ? "flex" : "none");
+  // }
+
+  function FirstSubMenu({ item, index }) {
+    return (
+      <CDropdown
+        component="div"
+        className="w-full h-full text-white bg-[#48322A] hover:bg-[#B82F15] duration-[0.4s]"
+      >
+        <CDropdownToggle
+          className={`w-full p-[0.7rem] flex items-center justify-between border border-solid border-[#48322A] hover:border-[#B82F15] ${
+            index !== 0 ? "border-t-[#CBCBCB] hover:border-t-[#CBCBCB]" : ""
+          }`}
+          // onClick={handleClickSubListItem}
+        >
+          <div className="flex items-center">
+            <i className="fas fa-caret-right fa-fw fa-lg mr-3" />
+            <span className="pl-[0.7rem] pr-[0.7rem] text-[1.1rem] hover:bg-[#1C8DD9]">
+              {item.title}
+            </span>
+          </div>
+          <FaPlus className="text-lg" />
+        </CDropdownToggle>
+        {/* <CDropdownMenu
+          className="absolute flex-col mt-[3.3rem] rounded bg-red-400"
+          style={{ display: isDisplaySub }}
+        >
+          {item.subMenu.map((item, index) => (
+            <FirstSubMenu key={index} item={item} index={index} />
+          ))}
+        </CDropdownMenu> */}
+      </CDropdown>
+    );
   }
 
   return (
@@ -69,7 +77,7 @@ export default function PrdListMenu() {
         style={{ display: isDisplay }}
       >
         {dataItems.map((item, index) => (
-          <FirstSubMenu key={index} item={item} />
+          <FirstSubMenu key={index} item={item} index={index} />
         ))}
       </CDropdownMenu>
     </>
