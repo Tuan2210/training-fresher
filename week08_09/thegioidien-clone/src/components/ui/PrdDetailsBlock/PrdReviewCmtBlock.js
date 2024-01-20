22ae44956d5304873c06b92614695d7125fb083e
Đinh Quang Tuấn <quangtuan496@gmail.com>
2024-01-21 00:08:10 +0700

    responsive PrdReviewCmtBlock

--- week08_09/thegioidien-clone/src/components/ui/PrdDetailsBlock/PrdReviewCmtBlock.js
+++ week08_09/thegioidien-clone/src/components/ui/PrdDetailsBlock/PrdReviewCmtBlock.js
@@ -1,6 +1,52 @@
 import styled from "styled-components";
 
+import { FaPaperPlane } from "react-icons/fa";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+
+import { REGEX_FULLNAME, REGEX_EMAIL } from "../../../constants/regexValidate";
+
+const CmtReviewSchema = yup.object().shape({
+  ["Họ tên"]: yup
+    .string()
+    .required("Vui lòng nhập họ tên!")
+    .test("is-fullname", "Họ tên không hợp lệ!", function (value) {
+      return yup
+        .string()
+        .matches(REGEX_FULLNAME, {
+          message: "Họ tên không hợp lệ!",
+          excludeEmptyString: true,
+        })
+        .isValidSync(value);
+    }),
+  ["Email"]: yup
+    .string()
+    .required("Vui lòng nhập email!")
+    .test("is-email", "Email không hợp lệ!", function (value) {
+      return yup
+        .string()
+        .matches(REGEX_EMAIL, {
+          message: "Email không hợp lệ!",
+          excludeEmptyString: true,
+        })
+        .isValidSync(value);
+    }),
+  ["Nội dung"]: yup.string().required("Vui lòng nhập nội dung!"),
+});
+
 export default function PrdReviewCmtBlock() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(CmtReviewSchema) });
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
   return (
     <StyledDiv className="bg-[#F9F9F9] mt-4 p-4 grid gap-2">
       {/* Đánh Giá Sản Phẩm */}
@@ -11,52 +57,78 @@ export default function PrdReviewCmtBlock() {
         </div>
       </div>
       {/* Form đánh giá */}
-      <form className="cmt-text flex flex-col mt-4 gap-2">
-        <div className="infoUserRow flex flex-col gap-2">
+      <form
+        className="cmt-text flex flex-col mt-4 gap-2"
+        onSubmit={handleSubmit((data) => onSubmit(data))}
+      >
+        <div className="info-user-row flex flex-wrap gap-2">
           {/* Họ tên */}
-          <div className="input-row__wrp w-full flex">
-            <input
-              type="text"
-              className="w-full p-2 text-[16px] border border-solid border-[#767676]"
-              placeholder="Họ tên..."
-              // {...register("fullName", {
-              //   required: "Vui lòng nhập họ tên!",
-              //   validate: handleValidateFullName,
-              // })}
-            />
-            <span className="err-alert text-[#FF6600] ml-2 mr-2 text-lg mt-2">
-              *
-            </span>
+          <div className="full-name-wrp w-full items-center flex flex-wrap">
+            <div className="w-full items-center flex">
+              <input
+                type="text"
+                className="w-full p-2 text-[16px] border border-solid border-[#767676] focus:bg-[#FFFFF6]"
+                placeholder="Họ tên..."
+                {...register("Họ tên")}
+              />
+              <span className="err-alert text-[#CC0000] ml-2 mr-2 text-lg mt-2">
+                *
+              </span>
+            </div>
+            {errors["Họ tên"] && (
+              <span className="text-[#CC0000] w-full">
+                {errors["Họ tên"]?.message}
+              </span>
+            )}
           </div>
           {/* Email */}
-          <div className="input-row__wrp w-full flex">
-            <input
+          <div className="email-wrp w-full items-center flex flex-wrap">
+            <div className="w-full items-center flex">
+              <input
+                type="text"
+                className="w-full p-2 text-[16px] border border-solid border-[#767676] focus:bg-[#FFFFF6]"
+                placeholder="Email..."
+                {...register("Email")}
+              />
+              <span className="err-alert text-[#CC0000] ml-2 mr-2 text-lg mt-2">
+                *
+              </span>
+            </div>
+            {errors.Email && (
+              <span className="text-[#CC0000] w-full">
+                {errors.Email.message}
+              </span>
+            )}
+          </div>
+        </div>
+        {/* Nội dung */}
+        <div className="content-cmt-row w-full items-center flex flex-wrap justify-start">
+          <div className="textarea-wrp w-full items-center flex">
+            <textarea
               type="text"
-              className="w-full p-2 text-[16px] border border-solid border-[#767676]"
-              placeholder="Email..."
-              // {...register("fullName", {
-              //   required: "Vui lòng nhập họ tên!",
-              //   validate: handleValidateFullName,
-              // })}
+              rows="3"
+              className="w-full p-2 text-[16px] border border-solid border-[#767676] focus:bg-[#FFFFF6] focus:outline-none"
+              placeholder="Nội dung..."
+              {...register("Nội dung")}
             />
-            <span className="err-alert text-[#FF6600] ml-2 mr-2 text-lg mt-2">
+            <span className="err-alert text-[#CC0000] ml-2 mr-2 text-lg mt-2">
               *
             </span>
           </div>
+          {errors["Nội dung"] && (
+            <span className="text-[#CC0000]">
+              {errors["Nội dung"]?.message}
+            </span>
+          )}
         </div>
-        <div className="contentCmtRow w-full flex">
-          <input
-            type="text"
-            className="w-full p-2 text-[16px] border border-solid border-[#767676]"
-            placeholder="Họ tên..."
-            // {...register("fullName", {
-            //   required: "Vui lòng nhập họ tên!",
-            //   validate: handleValidateFullName,
-            // })}
-          />
-          <span className="err-alert text-[#FF6600] ml-2 mr-2 text-lg mt-2">
-            *
-          </span>
+        <div className="send-cmt-wrp mt-2">
+          <button
+            type="submit"
+            className="sendCmtBtn flex items-center pt-2 pb-2 pl-[0.7rem] pr-[0.7rem] gap-2 text-[1.1rem] rounded-[3px] bg-[#646461]"
+          >
+            <FaPaperPlane className="text-[#FFFF00]" />
+            <span className="text-[#FAFAFA]">Gửi đánh giá</span>
+          </button>
         </div>
       </form>
       <div className="cmt-list"></div>
@@ -71,8 +143,36 @@ const StyledDiv = styled.div`
 
   @media screen and (min-width: 768px) {
     .cmt-text {
-      .infoUserRow {
-        flex-direction: row !important;
+      .info-user-row {
+        .full-name-wrp,
+        .email-wrp {
+          width: auto;
+          flex-wrap: nowrap;
+        }
+        input[type="text"] {
+          width: 320px;
+        }
+      }
+      .content-cmt-row {
+        .textarea-wrp {
+          width: 70% !important;
+          textarea {
+            width: 670px;
+          }
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 992px) {
+    .cmt-text {
+      .content-cmt-row {
+        .textarea-wrp {
+          width: 850px !important;
+          textarea {
+            width: 820px;
+          }
+        }
       }
     }
   }
